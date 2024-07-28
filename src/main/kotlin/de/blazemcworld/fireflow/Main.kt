3db2ee4e3c87f1ac179e905fd6739e4ab4f3ffd9
BlazeMCworld <de.blazemcworld@gmail.com>
2024-07-28 11:48:22 +0200

    cleanup node moving

--- src/main/kotlin/de/blazemcworld/fireflow/Main.kt
+++ src/main/kotlin/de/blazemcworld/fireflow/Main.kt
@@ -10,24 +10,16 @@ import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.BlockVec
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.coordinate.Vec
-import net.minestom.server.entity.EntityType
 import net.minestom.server.entity.GameMode
 import net.minestom.server.entity.Player
 import net.minestom.server.entity.attribute.Attribute
 import net.minestom.server.event.player.*
 import net.minestom.server.extras.MojangAuth
-import net.minestom.server.instance.Instance
-import net.minestom.server.instance.InstanceContainer
-import net.minestom.server.instance.InstanceManager
 import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.block.Block
-import net.minestom.server.timer.Scheduler
 import net.minestom.server.timer.TaskSchedule
 
 private val LOGGER = KotlinLogging.logger {}
-private var selectedNode: NodeComponent? = null
-private var cursorOffset: Pos2d? = null
-private var player: Player? = null
 
 fun main() {
     LOGGER.info { "Starting up FireFlow..." }
@@ -68,6 +60,7 @@ fun main() {
     }
 
     var creatingLine: IOComponent.Output? = null
+    var draggingNode: NodeComponent? = null
 
     val allNodes = mutableListOf<NodeComponent>()
     events.addListener(PlayerBlockInteractEvent::class.java) {
@@ -101,17 +94,29 @@ fun main() {
                     node.remove()
                     allNodes.remove(node)
                 } else if (it.player.heldSlot.toInt() == 7) {
-                    if (selectedNode == node) {
-                        selectedNode?.setOutlineColor(NamedTextColor.WHITE)
-                        selectedNode?.update(inst)
-                        selectedNode = null
+                    if (draggingNode == node) {
+                        node.setOutlineColor(NamedTextColor.WHITE)
+                        node.update(inst)
+                        draggingNode = null
                     } else {
+                        draggingNode = node
                         node.setOutlineColor(NamedTextColor.GREEN)
                         node.update(inst)
-                        cursorOffset = node.pos - cursor
-                        selectedNode = node
-                        player = it.player
-                        scheduler.scheduleNextTick { moveNode(inst, scheduler) }
+                        val offset = node.pos - cursor
+
+                        scheduler.submitTask task@{
+                            val movedCursor = it.player.cursorRaycast()
+                            if (it.player.heldSlot.toInt() == 7 && draggingNode == node) {
+                                node.pos = movedCursor + offset
+                                node.update(inst)
+                            } else {
+                                node.setOutlineColor(NamedTextColor.WHITE)
+                                node.update(inst)
+                                draggingNode = null
+                                return@task TaskSchedule.stop()
+                            }
+                            return@task TaskSchedule.tick(1)
+                        }
                     }
                 }
                 return@addListener
@@ -142,32 +147,12 @@ fun main() {
     LOGGER.info { "Ready!" }
 }
 
-fun rayCast(player: Player): Vec {
-    val norm = player.position.direction().dot(Vec(0.0, 0.0, -1.0))
-    if (norm >= 0) return Vec(0.0, 0.0, 0.0)
-    val start = Pos(player.position).asVec().add(0.0, player.eyeHeight, -16.0)
+private fun Player.cursorRaycast(): Pos2d {
+    val norm = position.direction().dot(Vec(0.0, 0.0, -1.0))
+    if (norm >= 0) return Pos2d.ZERO
+    val start = Pos(position).asVec().add(0.0, eyeHeight, -16.0)
     val dist = -start.dot(Vec(0.0, 0.0, -1.0)) / norm
-    if (dist < 0) return Vec(0.0, 0.0, 0.0)
-    return start.add(0.0, 0.0, 16.0).add(player.position.direction().mul(dist))
-}
-
-fun moveNode(inst: InstanceContainer, scheduler: Scheduler) {
-    if (player == null || selectedNode == null || cursorOffset == null) return
-
-    val raycast = rayCast(player!!)
-    val cursor = Pos2d(
-        raycast.x(),
-        raycast.y()
-    )
-
-    if (selectedNode != null && player?.heldSlot?.toInt() == 7) {
-        selectedNode!!.pos = cursor + cursorOffset!!
-        selectedNode!!.update(inst)
-        scheduler.scheduleNextTick { moveNode(inst, scheduler) }
-    } else {
-        selectedNode = null
-        cursorOffset = null
-        player = null
-        return
-    }
+    if (dist < 0) return Pos2d.ZERO
+    val out = start.add(position.direction().mul(dist))
+    return Pos2d(out.x, out.y)
 }
