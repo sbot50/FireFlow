6c6ea16402fe92165fdfdc8be448dfc3d40fef0f
BlazeMCworld <de.blazemcworld@gmail.com>
2024-07-28 09:40:53 +0200

    Improved node connections

--- src/main/kotlin/de/blazemcworld/fireflow/Main.kt
+++ src/main/kotlin/de/blazemcworld/fireflow/Main.kt
@@ -1,9 +1,8 @@
 package de.blazemcworld.fireflow
 
-import de.blazemcworld.fireflow.gui.LineComponent
+import de.blazemcworld.fireflow.gui.IOComponent
 import de.blazemcworld.fireflow.gui.NodeComponent
 import de.blazemcworld.fireflow.gui.Pos2d
-import de.blazemcworld.fireflow.gui.TextComponent
 import de.blazemcworld.fireflow.node.impl.NodeList
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.minestom.server.MinecraftServer
@@ -11,11 +10,7 @@ import net.minestom.server.coordinate.BlockVec
 import net.minestom.server.entity.GameMode
 import net.minestom.server.entity.Player
 import net.minestom.server.entity.attribute.Attribute
-import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
-import net.minestom.server.event.player.PlayerBlockBreakEvent
-import net.minestom.server.event.player.PlayerBlockInteractEvent
-import net.minestom.server.event.player.PlayerBlockPlaceEvent
-import net.minestom.server.event.player.PlayerSpawnEvent
+import net.minestom.server.event.player.*
 import net.minestom.server.extras.MojangAuth
 import net.minestom.server.instance.LightingChunk
 import net.minestom.server.instance.block.Block
@@ -37,7 +32,7 @@ fun main() {
         it.modifier().fill(
             BlockVec(0, 0, 0).add(it.absoluteStart()),
             BlockVec(16, 128, 1).add(it.absoluteStart()),
-            Block.SMOOTH_STONE
+            Block.POLISHED_BLACKSTONE
         )
     }
 
@@ -59,7 +54,7 @@ fun main() {
         it.isCancelled = true
     }
 
-    var creatingLine: TextComponent? = null
+    var creatingLine: IOComponent.Output? = null
 
     val allNodes = mutableListOf<NodeComponent>()
     events.addListener(PlayerBlockInteractEvent::class.java) {
@@ -76,13 +71,11 @@ fun main() {
                         return@addListener
                     }
                 }
-                creatingLine?.let { begin ->
+                creatingLine?.let { output ->
                     for (input in node.inputs) {
                         if (input.includes(cursor)) {
-                            val line = LineComponent()
-                            line.start = Pos2d(begin.pos.x, begin.pos.y + begin.height() * 0.75)
-                            line.end = Pos2d(input.pos.x + input.width(), input.pos.y + input.height() * 0.75)
-                            line.update(inst)
+                            input.connect(output)
+                            input.update(inst)
                             creatingLine = null
                             return@addListener
                         }
@@ -91,6 +84,7 @@ fun main() {
 
 
                 if (it.player.heldSlot.toInt() == 8) {
+                    creatingLine = null
                     node.remove()
                     allNodes.remove(node)
                 } else {
@@ -98,6 +92,16 @@ fun main() {
                     node.update(inst)
                 }
                 return@addListener
+            } else if (it.player.heldSlot.toInt() == 8) {
+                for (input in node.inputs) {
+                    for (line in input.lines) {
+                        if (line.distance(cursor) < 0.1) {
+                            input.connections.remove(input.lineOutputMap[line])
+                            input.update(inst)
+                            return@addListener
+                        }
+                    }
+                }
             }
         }
         if (it.player.heldSlot >= NodeList.all.size) return@addListener
--- src/main/kotlin/de/blazemcworld/fireflow/node/Node.kt
+++ src/main/kotlin/de/blazemcworld/fireflow/node/Node.kt
@@ -1,7 +1,7 @@
 package de.blazemcworld.fireflow.node
 
+import de.blazemcworld.fireflow.gui.IOComponent
 import de.blazemcworld.fireflow.gui.NodeComponent
-import de.blazemcworld.fireflow.gui.TextComponent
 import net.kyori.adventure.text.Component
 
 interface Node {
@@ -13,14 +13,10 @@ interface Node {
         val c = NodeComponent()
         c.title.text = Component.text(title)
         for (i in inputs) {
-            c.inputs.add(TextComponent().apply {
-                text = Component.text("○ " + i.name).color(i.type.color)
-            })
+            c.inputs.add(IOComponent.Input(i))
         }
         for (o in outputs) {
-            c.outputs.add(TextComponent().apply {
-                text = Component.text(o.name + " ○").color(o.type.color)
-            })
+            c.outputs.add(IOComponent.Output(o))
         }
         return c
     }
