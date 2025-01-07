f56a0f138c44203e0dd3a1e577d1b350dd56b3b2
Lexi Nuutraî <info@nuutrai.com>
2025-01-07 15:48:49 -0500

    Add optional support for getting snippets from files

--- src/main/java/de/blazemcworld/fireflow/command/SnippetCommand.java
+++ src/main/java/de/blazemcworld/fireflow/command/SnippetCommand.java
@@ -4,6 +4,7 @@ import de.blazemcworld.fireflow.FireFlow;
 import de.blazemcworld.fireflow.code.CodeEditor;
 import de.blazemcworld.fireflow.space.Space;
 import de.blazemcworld.fireflow.space.SpaceManager;
+import de.blazemcworld.fireflow.util.Config;
 import de.blazemcworld.fireflow.util.Translations;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -15,12 +16,15 @@ import net.minestom.server.command.builder.arguments.ArgumentString;
 import net.minestom.server.entity.Player;
 import net.minestom.server.timer.TaskSchedule;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 
 public class SnippetCommand extends Command {
@@ -40,30 +44,50 @@ public class SnippetCommand extends Command {
             if (editor == null) return;
             Player player = (Player) sender;
 
-            try (HttpClient client = HttpClient.newHttpClient()) {
-                client.sendAsync(HttpRequest.newBuilder()
-                                .uri(URI.create(ctx.get("url")))
-                                .timeout(Duration.ofSeconds(10))
-                                .build(), HttpResponse.BodyHandlers.ofInputStream())
-                        .thenAccept(res -> {
-                            if (res == null) return;
-                            try (InputStream stream = res.body()){
-                                byte[] bytes = stream.readNBytes(1048576);
-                                MinecraftServer.getSchedulerManager().scheduleTask(() -> {
-                                    if (player.getInstance() != editor.space.code) return;
+            if (Config.store.useFileURLs()) {
+                try {
+                    Path path = Path.of(ctx.get("url").toString());
+                    if (!path.isAbsolute()) {
+                        FireFlow.LOGGER.error("User provided non-absolute file path");
+                        player.sendMessage(Component.text(Translations.get("error.internal")).color(NamedTextColor.RED));
+                        return;
+                    }
+                    byte[] bytes = Files.readAllBytes(path);
+                    MinecraftServer.getSchedulerManager().scheduleTask(() -> {
+                       if (player.getInstance() != editor.space.code) return;
 
-                                    editor.placeSnippet((Player) sender, bytes);
-                                }, TaskSchedule.nextTick(), TaskSchedule.stop());
-                            } catch (IOException e) {
+                       editor.placeSnippet((Player) sender, bytes);
+                    }, TaskSchedule.nextTick(), TaskSchedule.stop());
+                } catch (IOException e) {
+                    FireFlow.LOGGER.error("Failed to read code snippet from " + ctx.get("url"), e);
+                    player.sendMessage(Component.text(Translations.get("error.internal")).color(NamedTextColor.RED));
+                }
+            } else {
+                try (HttpClient client = HttpClient.newHttpClient()) {
+                    client.sendAsync(HttpRequest.newBuilder()
+                                    .uri(URI.create(ctx.get("url")))
+                                    .timeout(Duration.ofSeconds(10))
+                                    .build(), HttpResponse.BodyHandlers.ofInputStream())
+                            .thenAccept(res -> {
+                                if (res == null) return;
+                                try (InputStream stream = res.body()) {
+                                    byte[] bytes = stream.readNBytes(1048576);
+                                    MinecraftServer.getSchedulerManager().scheduleTask(() -> {
+                                        if (player.getInstance() != editor.space.code) return;
+
+                                        editor.placeSnippet((Player) sender, bytes);
+                                    }, TaskSchedule.nextTick(), TaskSchedule.stop());
+                                } catch (IOException e) {
+                                    FireFlow.LOGGER.error("Failed to read code snippet from " + ctx.get("url"), e);
+                                    player.sendMessage(Component.text(Translations.get("error.internal")).color(NamedTextColor.RED));
+                                }
+                            })
+                            .exceptionally((e) -> {
                                 FireFlow.LOGGER.error("Failed to read code snippet from " + ctx.get("url"), e);
                                 player.sendMessage(Component.text(Translations.get("error.internal")).color(NamedTextColor.RED));
-                            }
-                        })
-                        .exceptionally((e) -> {
-                            FireFlow.LOGGER.error("Failed to read code snippet from " + ctx.get("url"), e);
-                            player.sendMessage(Component.text(Translations.get("error.internal")).color(NamedTextColor.RED));
-                            return null;
-                        });
+                                return null;
+                            });
+                }
             }
         }, new ArgumentLiteral("place"), new ArgumentString("url"));
     }
--- src/main/java/de/blazemcworld/fireflow/util/Config.java
+++ src/main/java/de/blazemcworld/fireflow/util/Config.java
@@ -32,7 +32,7 @@ public class Config {
         }
     }
 
-    public record Store(String motd, int port, LimitsConfig limits, String translations) {
+    public record Store(String motd, int port, boolean useFileURLs, LimitsConfig limits, String translations) {
     }
 
     public record LimitsConfig(long cpuUsage, int cpuHistory, int spacesPerPlayer, int totalSpaces, int spaceChunkDistance) {
